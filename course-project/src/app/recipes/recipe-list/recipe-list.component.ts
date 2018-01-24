63ada8c3bb413f87e164a7a9c1dbbdfee2be86e8
Carlos Carvajal <ing.carlosj@gmail.com>
2018-01-23 22:40:12 -0500

    New notes added

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes : Recipe[] = [
     new Recipe('A test recipe','This is a simply test',
-    'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg');
+    'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg')
   ];
 
   constructor() {}
